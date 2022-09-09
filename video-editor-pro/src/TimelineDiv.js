fc61f93c3db42bc4a0eccd4c657d9afb974da2f6
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2022-09-09 16:06:50 +0000

    latest

--- video-editor-pro/src/TimelineDiv.js
+++ video-editor-pro/src/TimelineDiv.js
@@ -3,7 +3,9 @@ import timelinediv from './css/timelinediv.module.css'
 function TimelineDiv()
 {
     return (
-        <div className={timelinediv.area}></div>
+        <div className={timelinediv.area}>
+            
+        </div>
     )
 }
 
--- video-editor-pro/src/ToolsDiv.js
+++ video-editor-pro/src/ToolsDiv.js
@@ -1,9 +1,17 @@
 import toolsdiv from './css/toolsdiv.module.css'
+import {MdOutlineInvertColors} from 'react-icons/md'
+import {BiCut} from 'react-icons/bi'
 
 function ToolsDiv()
 {
     return(
-        <div className={toolsdiv.area}></div>
+        <div className={toolsdiv.area}>
+            <MdOutlineInvertColors size={30} color="white"/>
+            <p>Grey Scale</p>
+
+            <BiCut size={30} color="white" />
+            <p>Cut</p>
+        </div>
     )
 }
 
