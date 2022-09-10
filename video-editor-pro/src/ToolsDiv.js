210af56f04fee368e602cd01a963a5981e76fb28
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2022-09-10 04:22:32 +0000

    toolsdiv ui

--- video-editor-pro/src/ToolsDiv.js
+++ video-editor-pro/src/ToolsDiv.js
@@ -1,18 +1,36 @@
-import toolsdiv from './css/toolsdiv.module.css'
-import {MdOutlineInvertColors} from 'react-icons/md'
-import {BiCut} from 'react-icons/bi'
+import toolsdiv from "./css/toolsdiv.module.css";
+import { MdOutlineInvertColors } from "react-icons/md";
+import { BiCut } from "react-icons/bi";
+import {AiOutlineRotateRight, AiOutlineMergeCells} from 'react-icons/ai'
 
-function ToolsDiv()
-{
-    return(
-        <div className={toolsdiv.area}>
-            <MdOutlineInvertColors size={30} color="white"/>
-            <p>Grey Scale</p>
+function ToolsDiv() {
 
-            <BiCut size={30} color="white" />
-            <p>Cut</p>
-        </div>
-    )
+  return (
+    <div className={toolsdiv.area}>
+
+      <button className={toolsdiv.toolsButton}>
+        <MdOutlineInvertColors size={30} color="white" />
+        <p>Grey Scale</p>
+      </button>
+
+      <button className={toolsdiv.toolsButton}>
+        <BiCut size={30} color="white" />
+        <p>Cut</p>
+      </button>
+
+      <button className={toolsdiv.toolsButton}>
+        <AiOutlineRotateRight size={30} color="white" />
+        <p>Rotate</p>
+      </button>
+
+
+      <button className={toolsdiv.toolsButton}>
+        <AiOutlineMergeCells size={30} color="white" />
+        <p>Merge</p>
+      </button>
+
+    </div>
+  );
 }
 
-export default ToolsDiv
+export default ToolsDiv;
