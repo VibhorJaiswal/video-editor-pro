f4d395c232cd6a6770a04cd8f97d9e33d12b8c9c
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2022-10-18 16:30:36 +0000

    working

--- video-editor-pro/src/App.js
+++ video-editor-pro/src/App.js
@@ -7,6 +7,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
+  const universalUrl =
+    "https://5000-vibhorjaisw-videoeditor-fz8qdtoc0iv.ws-us71.gitpod.io";
+
   const [currentPage, changeCurrentPage] = useState(true); //false for login page
   const [uploadedVideo, changeUploadedVideo] = useState({});
   const [username, changeusername] = useState("baman");
@@ -35,8 +38,7 @@ function App() {
   useEffect(() => {
     if (uploadedVideo === {}) return;
 
-    const url =
-      "https://5000-vibhorjaisw-videoeditor-1846ju1x2au.ws-us71.gitpod.io/upload";
+    const url = `${universalUrl}/upload`;
 
     // console.log('upload video: ',uploadedVideo);
 
@@ -56,15 +58,13 @@ function App() {
       });
   }, [uploadedVideo]);
 
-
   useEffect(() => {
-    console.log('filename: ',currentVideo);
+    console.log("filename: ", currentVideo);
   }, [currentVideo]);
-  
+
   //Temp function for development. To be removed.
   useEffect(() => {
-    const urlCreateUser =
-      "https://5000-vibhorjaisw-videoeditor-1846ju1x2au.ws-us71.gitpod.io/createnewuser";
+    const urlCreateUser = `${universalUrl}/createnewuser`;
     fetch(urlCreateUser, {
       method: "POST", // or 'PUT'
       headers: {
@@ -79,41 +79,29 @@ function App() {
   //To perform editing
   useEffect(() => {
     if (editType != "none") {
-      
-      console.log('performing: ', editType);
-      
-      const data  = {
+      console.log("performing: ", editType);
+
+      const data = {
         username,
-        "command":editType,
-        "input1":currentVideo,
-        "output":`${currentVideo.replace('.mp4', '')}_output.mp4`
-      }
+        command: editType,
+        input1: currentVideo,
+        output: `${currentVideo.replace(".mp4", "")}_output.mp4`,
+      };
 
-
-    fetch('https://5000-vibhorjaisw-videoeditor-1846ju1x2au.ws-us71.gitpod.io/doTranscode', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(() => {
-        let videoPlayer = document.getElementById('currentVideoPlayer');
-        let videoUrl = `https://5000-vibhorjaisw-videoeditor-1846ju1x2au.ws-us71.gitpod.io/${username}/${currentVideo.replace('.mp4', '')}_output.mp4`
+      fetch(`${universalUrl}/doTranscode`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then(() => {
+        let videoPlayer = document.getElementById("currentVideoPlayer");
+        let videoUrl = `${universalUrl}/${username}/${currentVideo.replace(
+          ".mp4",
+          ""
+        )}_output.mp4`;
         videoPlayer.src = videoUrl;
-      })
-
-    // const video =   document.querySelector("video")
-    // video.src = "http://localhost:5000/user1/output.mp4";
-    // video.load();
-    // video.play();
-
-    //   console.log(data);
-
-
-
-
-
+      });
     }
   }, [editType]);
 
